958809d434bebb14f575394ce465a12a29a882da
Artem Meldziuk <retrosexyal@gmail.com>
2023-04-28 21:47:49 +0300

    fix cron

--- pages/api/sendmailer.ts
+++ pages/api/sendmailer.ts
@@ -1,11 +1,18 @@
 import { mailOptionsBirthday, transporter } from "@/config/nodemailer";
+import { NextApiRequest, NextApiResponse } from "next";
 
 console.log("скрипт запущен");
 
-transporter.sendMail({
-  ...mailOptionsBirthday,
-  subject: "Анкета",
-  text: "zzz",
-  html: `<h1>авто</h1><h2>авто</h2><h2>авто</h2>`,
-});
-console.log("письмо отправлено");
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  await transporter.sendMail({
+    ...mailOptionsBirthday,
+    subject: "Анкета",
+    text: "zzz",
+    html: `<h1>авто</h1><h2>авто</h2><h2>авто</h2>`,
+  });
+  console.log("письмо отправлено");
+  res.status(200).json({ message: "письмо отправлено" });
+}
--- server.ts
+++ server.ts
@@ -1,7 +1,13 @@
-import cron from "node-cron";
-import { transporter, mailOptionsBirthday } from "./config/nodemailer";
+/* import cron from "node-cron";
+import { transporter, mailOptionsBirthday } from "./config/nodemailer.js";
 
 console.log("скрипт запущен");
+transporter.sendMail({
+  ...mailOptionsBirthday,
+  subject: "Анкета",
+  text: "zzz",
+  html: `<h1>авто</h1><h2>авто</h2><h2>авто</h2>`,
+});
 
 cron.schedule(
   "20 19 * * *",
@@ -18,4 +24,26 @@ cron.schedule(
     scheduled: true,
     timezone: "Europe/Moscow",
   }
-);
+); */
+
+import { createServer } from 'http'
+import { parse } from 'url'
+import next from 'next'
+
+const port = parseInt(process.env.PORT || '3000', 10)
+const dev = process.env.NODE_ENV !== 'production'
+const app = next({ dev })
+const handle = app.getRequestHandler()
+
+app.prepare().then(() => {
+  createServer((req, res) => {
+    const parsedUrl = parse(req.url!, true)
+    handle(req, res, parsedUrl)
+  }).listen(port)
+
+  console.log(
+    `> Server listening at http://localhost:${port} as ${
+      dev ? 'development' : process.env.NODE_ENV
+    }`
+  )
+})
